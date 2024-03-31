d5e43e08ccdd35eaac3fb96943b9a4c5cd85255d
covaleski <covaleski23@gmail.com>
2024-03-31 14:51:29 -0300

    feat: create `MigrationFile`

---
+++ src/Migration/MigrationFile.php
@@ -0,0 +1,126 @@
+<?php
+
+/**
+ * This file is part of Laucov's Modeling Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package modeling
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\Modeling\Migration;
+
+/**
+ * Represents a database migration file.
+ */
+class MigrationFile
+{
+    /**
+     * Migration date.
+     */
+    public readonly \DateTimeImmutable $date;
+
+    /**
+     * Migration name.
+     */
+    public readonly string $name;
+
+    /**
+     * Create the migration file instance.
+     */
+    public function __construct(
+        /**
+         * Migration filename.
+         */
+        public readonly string $filename,
+
+        string $time_format,
+    ) {
+        // Validate file basename.
+        $basename = basename($filename);
+        if (preg_match('/^.+\-\w+(\.\w+)+$/', $basename) !== 1) {
+            $message = 'Migration filenames must have the format "%s".';
+            $message = sprintf($message, '{datetime}_{classname}.php');
+            throw new \InvalidArgumentException($message);
+        }
+
+        // Check if filename exists.
+        if (!file_exists($filename)) {
+            $message = "Migration file {$filename} does not exist.";
+            throw new \RuntimeException($message);
+        }
+
+        // Get the last hyphen position.
+        $split_at = strrpos($basename, '-');
+
+        // Set date.
+        $datetime = substr($basename, 0, $split_at);
+        $date = \DateTimeImmutable::createFromFormat($time_format, $datetime);
+        if (is_bool($date)) {
+            $message = 'Migration datetime does not match the format %s.';
+            throw new \RuntimeException(sprintf($message, $time_format));
+        }
+        $this->date = $date;
+
+        // Set name.
+        $this->name = explode('.', substr($basename, $split_at + 1))[0];
+    }
+
+    /**
+     * Get the migration actual class name.
+     */
+    public function getClassName(): null|string
+    {
+        // Get tokens.
+        $tokens = token_get_all(file_get_contents($this->filename));
+
+        // Get namespace and name.
+        $namespace = '';
+        $waiting_namespace = false;
+        $waiting_name = false;
+        foreach ($tokens as $token) {
+            // Check if isn't just a character
+            if (!is_array($token)) {
+                continue;
+            }
+            // Get token name.
+            $name = token_name($token[0]);
+            if ($name === 'T_NAMESPACE') {
+                // Start waiting for a namespace.
+                $waiting_namespace = true;
+            } elseif ($name === 'T_NAME_QUALIFIED' && $waiting_namespace) {
+                // Register the migration namespace.
+                $namespace = $token[1];
+                $waiting_namespace = false;
+            } elseif ($name === 'T_CLASS') {
+                // Start waiting for a class name.
+                $waiting_name = true;
+            } elseif ($name === 'T_STRING' && $waiting_name) {
+                // Register the class name with the current namespace.
+                return strlen($namespace) > 0
+                    ? ($namespace . '\\' . $token[1])
+                    : $token[1];
+            }
+        }
+
+        return null;
+    }
+}
---
+++ tests/unit/Migration/MigrationFileTest.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * This file is part of Laucov's Modeling Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package modeling
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Migration;
+
+use Laucov\Modeling\Migration\MigrationFile;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Laucov\Modeling\Migration\MigrationFile
+ */
+class MigrationFileTest extends TestCase
+{
+    /**
+     * @covers ::__construct
+     * @covers ::getClassName
+     */
+    public function testCanCreateFromFilename(): void
+    {
+        // Create instance.
+        $directory = __DIR__ . '/migration-files';
+        $filename = "{$directory}/2023-03-31-105000-CreateFlightsTable.php";
+        $migration = new MigrationFile($filename, 'Y-m-d-His');
+        
+        // Get filename, name and datetime.
+        $this->assertSame($filename, $migration->filename);
+        $this->assertSame('CreateFlightsTable', $migration->name);
+        $time = '31/03/2023 10:50:00 +00:00';
+        $this->assertSame($time, $migration->date->format('d/m/Y H:i:s P'));
+
+        // Get real class name.
+        $class_name = 'Tests\Unit\Migration\CreateFlightsTable';
+        $this->assertSame($class_name, $migration->getClassName());
+
+        // Create migration with no class (invalid).
+        $filename = "{$directory}/2023-03-31-125100-InvalidMigration.php";
+        $migration = new MigrationFile($filename, 'Y-m-d-His');
+        $this->assertNull($migration->getClassName());
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testFileMustExist(): void
+    {
+        $this->expectException(\RuntimeException::class);
+        new MigrationFile('/path/to/2024-InexistentMigration.php', 'Y');
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testFilenameMustHaveAValidSynthax(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        new MigrationFile("/path/to/invalid_migration.php", '');
+    }
+
+    /**
+     * @covers ::__construct
+     */
+    public function testFilenamePrefixMustMatchDateFormat(): void
+    {
+        $filename = __DIR__ . '/migration-files'
+            . '/2023-03-31-105000-CreateFlightsTable.php';
+        $this->expectException(\RuntimeException::class);
+        new MigrationFile($filename, 'YmdHis');
+    }
+}
