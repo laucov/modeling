34fa8133622ee8a55ea8b21f501dd48115c27bdf
covaleski <covaleski23@gmail.com>
2024-05-03 12:46:11 -0300

    feat: create `Required` and `RequiredWith` attributes

---
+++ src/Entity/Required.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * This file is part of Laucov's Modeling Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package modeling
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\Modeling\Entity;
+
+/**
+ * Indicates that an entity property is required.
+ */
+#[\Attribute(\Attribute::TARGET_PROPERTY)]
+class Required
+{
+}
---
+++ tests/unit/Entity/RequiredTest.php
@@ -0,0 +1,53 @@
+<?php
+
+/**
+ * This file is part of Laucov's Modeling Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package modeling
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Entity;
+
+use Laucov\Modeling\Entity\Required;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Laucov\Modeling\Entity\Required
+ */
+class RequiredTest extends TestCase
+{
+    /**
+     * @coversNothing
+     */
+    public function testCanUseAsAttribute(): void
+    {
+        $this->expectNotToPerformAssertions();
+
+        new class {
+            #[Required]
+            public $a = 'foo';
+        };
+    }
+}
