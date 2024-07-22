9e26ca5823728e6ac5087992f9fb630f8496625b
covaleski <covaleski23@gmail.com>
2024-07-22 14:14:40 -0300

    feat: create `EntityValidator`

---
+++ src/Validation/EntityValidator.php
@@ -0,0 +1,236 @@
+<?php
+
+/**
+ * This file is part of Laucov's Modeling Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package modeling
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+namespace Laucov\Modeling\Validation;
+
+use Laucov\Modeling\Entity\AbstractEntity;
+use Laucov\Modeling\Entity\ErrorMessage;
+use Laucov\Modeling\Entity\Required;
+use Laucov\Validation\Error;
+use Laucov\Validation\Interfaces\RuleInterface;
+use Laucov\Validation\Ruleset;
+
+/**
+ * Validates entities.
+ */
+class EntityValidator
+{
+    /**
+     * Active entity.
+     */
+    protected AbstractEntity $entity;
+
+    /**
+     * Errors found.
+     * 
+     * @var array<string, RuleInterface[]>
+     */
+    protected array $errors = [];
+
+    /**
+     * Reflection properties.
+     * 
+     * @var array<\ReflectionProperty>
+     */
+    protected array $properties;
+
+    /**
+     * Property names.
+     * 
+     * @var array<string>
+     */
+    protected array $propertyNames;
+
+    /**
+     * Cached property rules.
+     * 
+     * @var array<string, Ruleset>
+     */
+    protected array $rules;
+
+    /**
+     * Get all invalid properties names.
+     * 
+     * @return array<string>
+     */
+    public function getErrorKeys(): array
+    {
+        return array_keys($this->errors);
+    }
+
+    /**
+     * Get current errors.
+     * 
+     * @return array<Error>
+     */
+    public function getErrors(string $property_name): array
+    {
+        return $this->errors[$property_name] ?? [];
+    }
+
+    /**
+     * Check if the entity has errors.
+     */
+    public function hasErrors(null|string $name = null): bool
+    {
+        return $name !== null
+            ? (isset($this->errors[$name]) && count($this->errors[$name]))
+            : count($this->errors);
+    }
+
+    /**
+     * Set the current entity.
+     */
+    public function setEntity(AbstractEntity $entity): static
+    {
+        if (isset($this->entity) && $entity::class !== $this->entity::class) {
+            unset($this->rules);
+        }
+        $this->entity = $entity;
+        return $this;
+    }
+
+    /**
+     * Validate current values.
+     */
+    public function validate(): bool
+    {
+        // Reset errors and get stored values.
+        $this->errors = [];
+
+        // Validate each value.
+        foreach ($this->getPropertyNames() as $name) {
+            $value = $this->entity->$name ?? null;
+            $ruleset = $this->getRuleset($name);
+            if (!$ruleset->validate($value)) {
+                $this->errors[$name] = $ruleset->getErrors();
+            }
+        }
+
+        return count($this->errors) === 0;
+    }
+
+    /**
+     * Cache the entity's rules.
+     */
+    protected function cacheRules(): void
+    {
+        // Extract each properties' rules.
+        foreach ($this->getProperties() as $prop) {
+            // Get name.
+            $name = $prop->getName();
+            // Get attributes.
+            $attributes = $prop->getAttributes();
+            // Create ruleset.
+            $ruleset = new Ruleset();
+            $ruleset->setData($this->entity);
+            $this->rules[$name] = $ruleset;
+            // Process attributes.
+            $is_required = false;
+            $required_with = null;
+            $obligatoriness_message = null;
+            /** @var null|string|RuleInterface */
+            $rule = null;
+            foreach ($attributes as $attr) {
+                if (is_a($attr->getName(), Required::class, true)) {
+                    // Remember obligatoriness.
+                    /** @var Required */
+                    $attr = $attr->newInstance();
+                    $rule = 'required';
+                    $is_required = true;
+                    if (count($attr->with)) {
+                        $required_with = $attr->with;
+                    }
+                } elseif (is_a($attr->getName(), RuleInterface::class, true)) {
+                    // Add rule.
+                    /** @var RuleInterface */
+                    $attr = $attr->newInstance();
+                    $ruleset->addRule($attr);
+                    $rule = $attr;
+                } elseif (is_a($attr->getName(), ErrorMessage::class, true)) {
+                    // Add message.
+                    /** @var ErrorMessage */
+                    $attr = $attr->newInstance();
+                    if ($rule === 'required') {
+                        $obligatoriness_message = $attr->content;
+                    } elseif ($rule instanceof RuleInterface) {
+                        $rule->setMessage($attr->content);
+                    }
+                }
+            }
+            // Set obligatoriness.
+            if ($is_required) {
+                $ruleset->require($required_with, $obligatoriness_message);
+            }
+        }
+    }
+
+    /**
+     * Get all the entity's public property reflections.
+     * 
+     * @return array<\ReflectionProperty>
+     */
+    protected function getProperties(): array
+    {
+        // Cache properties.
+        if (!isset($this->properties)) {
+            $reflection = new \ReflectionClass($this->entity::class);
+            $filter = \ReflectionProperty::IS_PUBLIC;
+            $this->properties = $reflection->getProperties($filter);
+        }
+
+        return $this->properties;
+    }
+
+    /**
+     * Get all the entity's public property names.
+     */
+    protected function getPropertyNames(): array
+    {
+        // Cache property names.
+        if (!isset($this->propertyNames)) {
+            $props = $this->getProperties();
+            $this->propertyNames = array_map(fn ($p) => $p->getName(), $props);
+        }
+
+        return $this->propertyNames;
+    }
+
+    /**
+     * Get rules for a specific property.
+     */
+    protected function getRuleset(string $property_name): Ruleset
+    {
+        // Cache rules.
+        if (!isset($this->rules)) {
+            $this->cacheRules();
+        }
+
+        return $this->rules[$property_name];
+    }
+}
---
+++ tests/unit/Validation/EntityValidatorTest.php
@@ -0,0 +1,355 @@
+<?php
+
+/**
+ * This file is part of Laucov's Modeling Library project.
+ * 
+ * Copyright 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * @package modeling
+ * 
+ * @author Rafael Covaleski Pereira <rafael.covaleski@laucov.com>
+ * 
+ * @license <http://www.apache.org/licenses/LICENSE-2.0> Apache License 2.0
+ * 
+ * @copyright © 2024 Laucov Serviços de Tecnologia da Informação Ltda.
+ */
+
+declare(strict_types=1);
+
+namespace Tests\Unit\Entity;
+
+use Laucov\Modeling\Entity\AbstractEntity;
+use Laucov\Modeling\Entity\ErrorMessage;
+use Laucov\Modeling\Entity\Required;
+use Laucov\Modeling\Validation\EntityValidator;
+use Laucov\Validation\Rules\Length;
+use Laucov\Validation\Rules\Regex;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @coversDefaultClass \Laucov\Modeling\Validation\EntityValidator
+ */
+class EntityValidatorTest extends TestCase
+{
+    /**
+     * Validator instance.
+     */
+    protected EntityValidator $validator;
+
+    /**
+     * @covers ::cacheRules
+     * @covers ::getRuleset
+     * @uses Laucov\Modeling\Entity\AbstractEntity::__construct
+     * @uses Laucov\Modeling\Validation\EntityValidator::getProperties
+     * @uses Laucov\Modeling\Validation\EntityValidator::getPropertyNames
+     * @uses Laucov\Modeling\Validation\EntityValidator::getRuleset
+     * @uses Laucov\Modeling\Validation\EntityValidator::setEntity
+     * @uses Laucov\Modeling\Validation\EntityValidator::validate
+     * @uses Laucov\Modeling\Entity\Required::__construct
+     */
+    public function testCachesRules(): void
+    {
+        // Create validator.
+        // Override the `cacheRules()` method to test whether the entity caches
+        // its rules instead of repeatedly fetching them as attributes.
+        $validator = new class () extends EntityValidator {
+            public static int $cacheCount = 0;
+            protected function cacheRules(): void
+            {
+                static::$cacheCount++;
+                parent::cacheRules();
+            }
+        };
+
+        // Create entity.
+        $entity = new class () extends AbstractEntity {
+            #[Required]
+            #[Regex('/^\d+\-\d+$/')]
+            public string $zip_code;
+            #[Required(['foo', 'bar'])]
+            #[Regex('/^[A-Z]+$/')]
+            #[Length(3, 3)]
+            public string $country;
+        };
+        $class_name = $entity::class;
+
+        // Test caching with the same entity.
+        $validator->setEntity($entity);
+        $validator->validate();
+        $validator->validate();
+        $validator->validate();
+        $validator->setEntity($entity);
+        $validator->validate();
+        $validator->validate();
+        $validator->validate();
+        $this->assertSame(1, $validator::class::$cacheCount);
+
+        // Test caching with entity of same type.
+        $validator->setEntity(new $class_name());
+        $validator->validate();
+        $validator->validate();
+        $validator->validate();
+        $this->assertSame(1, $validator::class::$cacheCount);
+
+        // Create other entity.
+        $other_entity = new class () extends AbstractEntity {
+            #[Required]
+            public string $name;
+            #[Required]
+            #[Length(2, 2)]
+            public int $age;
+        };
+
+        // Test caching with entity of another type.
+        $validator->setEntity($other_entity);
+        $validator->validate();
+        $validator->validate();
+        $validator->validate();
+        $this->assertSame(2, $validator::class::$cacheCount);
+    }
+
+    /**
+     * @covers ::cacheRules
+     * @uses Laucov\Modeling\Entity\AbstractEntity::__construct
+     * @uses Laucov\Modeling\Validation\EntityValidator::getErrors
+     * @uses Laucov\Modeling\Validation\EntityValidator::getProperties
+     * @uses Laucov\Modeling\Validation\EntityValidator::getPropertyNames
+     * @uses Laucov\Modeling\Validation\EntityValidator::getRuleset
+     * @uses Laucov\Modeling\Validation\EntityValidator::setEntity
+     * @uses Laucov\Modeling\Validation\EntityValidator::validate
+     * @uses Laucov\Modeling\Entity\ErrorMessage::__construct
+     * @uses Laucov\Modeling\Entity\Required::__construct
+     */
+    public function testCanSetValidationMessages(): void
+    {
+        // Create entity instance.
+        $entity = new class () extends AbstractEntity {
+            #[Required]
+            #[Length(0, 16)]
+            public string $name_a;
+            #[Required]
+            #[ErrorMessage('This field is required!')]
+            #[Length(0, 16)]
+            #[ErrorMessage('Value too long!')]
+            public string $name_b;
+        };
+
+        // Test "Required" error.
+        $this->validator
+            ->setEntity($entity)
+            ->validate();
+        $errors_a = $this->validator->getErrors('name_a');
+        $this->assertCount(1, $errors_a);
+        $this->assertSame('required', $errors_a[0]->rule);
+        $this->assertNull($errors_a[0]->message);
+        $errors_b = $this->validator->getErrors('name_b');
+        $this->assertCount(1, $errors_b);
+        $this->assertSame('required', $errors_b[0]->rule);
+        $this->assertSame('This field is required!', $errors_b[0]->message);
+
+        // Test rule error.
+        $entity->name_a = 'Very long value...';
+        $entity->name_b = 'Another very long value...';
+        $this->validator->validate();
+        $errors_a = $this->validator->getErrors('name_a');
+        $this->assertCount(1, $errors_a);
+        $this->assertSame(Length::class, $errors_a[0]->rule);
+        $this->assertNull($errors_a[0]->message);
+        $errors_b = $this->validator->getErrors('name_b');
+        $this->assertCount(1, $errors_b);
+        $this->assertSame(Length::class, $errors_b[0]->rule);
+        $this->assertSame('Value too long!', $errors_b[0]->message);
+    }
+
+    /**
+     * @covers ::hasErrors
+     * @covers ::getErrorKeys
+     * @covers ::getErrors
+     * @covers ::getProperties
+     * @covers ::getPropertyNames
+     * @covers ::validate
+     * @uses Laucov\Modeling\Entity\AbstractEntity::__construct
+     * @uses Laucov\Modeling\Entity\AbstractEntity::cache
+     * @uses Laucov\Modeling\Entity\AbstractEntity::toArray
+     * @uses Laucov\Modeling\Validation\EntityValidator::cacheRules
+     * @uses Laucov\Modeling\Validation\EntityValidator::getRuleset
+     * @uses Laucov\Modeling\Validation\EntityValidator::setEntity
+     * @uses Laucov\Modeling\Entity\ObjectReader::toArray
+     * @uses Laucov\Modeling\Entity\Required::__construct
+     * @uses Laucov\Validation\Rules\Length::__construct
+     * @uses Laucov\Validation\Rules\Length::validate
+     * @uses Laucov\Validation\Rules\Regex::__construct
+     * @uses Laucov\Validation\Rules\Regex::validate
+     * @uses Laucov\Validation\Ruleset::addRule
+     * @uses Laucov\Validation\Ruleset::getErrors
+     * @uses Laucov\Validation\Ruleset::validate
+     */
+    public function testCanValidate(): void
+    {
+        // Create entity instance.
+        $entity = new class () extends AbstractEntity {
+            #[Length(8, 16)]
+            public string $login;
+            #[Length(16, 24)]
+            #[Regex('/[A-Z]+/')]
+            #[Regex('/[a-z]+/')]
+            #[Regex('/\d+/')]
+            #[Regex('/[\!\#\$\%\&\@]+/')]
+            public string $password;
+            public bool $has_email;
+            #[Required(['has_email'])]
+            public string $email;
+        };
+
+        // Validate valid values.
+        $entity->login = 'john.doe';
+        $entity->password = 'Secret_Pass#1234';
+        $this->assertValidation($entity, []);
+
+        // Set invalid value.
+        $entity->login = 'john.manoel.foobar.doe';
+        $this->assertValidation($entity, ['login' => [Length::class]]);
+
+        // Fix and add another invalid value.
+        $entity->login = 'john.foobar';
+        $entity->password = 'ABCDEF';
+        $this->assertValidation($entity, [
+            'password' => [
+                Length::class,
+                Regex::class,
+                Regex::class,
+                Regex::class,
+            ],
+        ]);
+
+        // Fix again.
+        $entity->password = 'SECUREpass@987654321';
+        $this->assertValidation($entity, []);
+
+        // Test context data.
+        $entity->has_email = true;
+        $this->assertValidation($entity, ['email' => ['required_with']]);
+    }
+
+    /**
+     * @covers ::validate
+     * @uses Laucov\Modeling\Entity\AbstractEntity::__construct
+     * @uses Laucov\Modeling\Validation\EntityValidator::cacheRules
+     * @uses Laucov\Modeling\Validation\EntityValidator::getErrorKeys
+     * @uses Laucov\Modeling\Validation\EntityValidator::getErrors
+     * @uses Laucov\Modeling\Validation\EntityValidator::getProperties
+     * @uses Laucov\Modeling\Validation\EntityValidator::getPropertyNames
+     * @uses Laucov\Modeling\Validation\EntityValidator::getRuleset
+     * @uses Laucov\Modeling\Validation\EntityValidator::hasErrors
+     * @uses Laucov\Modeling\Validation\EntityValidator::setEntity
+     * @uses Laucov\Modeling\Entity\Required::__construct
+     */
+    public function testValidatesUnsetProperties(): void
+    {
+        // Create entity instance.
+        $entity = new class () extends AbstractEntity {
+            #[Required]
+            public string $first_name;
+            #[Required]
+            public string $last_name;
+        };
+
+        // Validate.
+        $this->assertValidation($entity, [
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+        ]);
+    }
+
+    /**
+     * Assert that the entity contains the given errors.
+     */
+    protected function assertValidation(
+        AbstractEntity $entity,
+        array $expected_errors = [],
+    ): void {
+        // Compare validation result.
+        $expected_success = count($expected_errors) < 1;
+        $actual_success = $this->validator
+            ->setEntity($entity)
+            ->validate();
+        $message = 'Assert that the entity data %s valid.';
+        $message = sprintf($message, $expected_success ? 'is' : 'is not');
+        $this->assertSame($expected_success, $actual_success, $message);
+
+        // Assert that has errors.
+        if ($expected_success) {
+            $message = 'Assert that the entity doesn\'t have errors.';
+            $this->assertFalse($this->validator->hasErrors(), $message);
+        } else {
+            $message = 'Assert that the entity has errors.';
+            $this->assertTrue($this->validator->hasErrors(), $message);
+        }
+
+        // Check error keys.
+        $expected_keys = array_keys($expected_errors);
+        $actual_keys = $this->validator->getErrorKeys();
+        $missing = array_diff($expected_keys, $actual_keys);
+        if (count($missing) > 0) {
+            $message = 'Missing entity error keys: %s.';
+            $this->fail(sprintf($message, implode(', ', $missing)));
+        }
+        $unexpected = array_diff($actual_keys, $expected_keys);
+        if (count($unexpected) > 0) {
+            $message = 'Found unexpected entity error keys: %s.';
+            $this->fail(sprintf($message, implode(', ', $unexpected)));
+        }
+
+        // Get public property names.
+        $reflection = new \ReflectionObject($entity);
+        $filter = \ReflectionProperty::IS_PUBLIC;
+        $properties = $reflection->getProperties($filter);
+        $property_names = array_map(fn ($p) => $p->getName(), $properties);
+
+        // Compare property names.
+        $err_tpl = 'Failed to assert that entity property "%s" %s errors.';
+        foreach ($property_names as $name) {
+            $has_errors = $this->validator->hasErrors($name);
+            $expects_errors = array_key_exists($name, $expected_errors);
+            if ($has_errors && !$expects_errors) {
+                $this->fail(sprintf($err_tpl, $name, 'does not have'));
+            } elseif (!$has_errors && $expects_errors) {
+                $this->fail(sprintf($err_tpl, $name, 'has'));
+            }
+        }
+
+        // Check property errors.
+        foreach ($expected_errors as $name => $expected_classes) {
+            $actual_errors = $this->validator->getErrors($name);
+            $this->assertIsArray($actual_errors);
+            $msg_tpl = 'Assert that entity property "%s" #%s error is %s.';
+            foreach ($expected_classes as $i => $expected_class) {
+                $message = sprintf($msg_tpl, $name, $i, $expected_class);
+                $actual_class = $actual_errors[$i]->rule;
+                $this->assertSame($expected_class, $actual_class, $message);
+            }
+            // Check error list size.
+            $this->assertSameSize($expected_classes, $actual_errors);
+        }
+    }
+
+    /**
+     * This method is called before each test.
+     */
+    protected function setUp(): void
+    {
+        $this->validator = new EntityValidator();
+    }
+}
